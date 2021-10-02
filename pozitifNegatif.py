dae1fff408c5be17942d85354fb2d1c0f2b96a7c
Sercan Efe Karaman <70342545+sercanefe@users.noreply.github.com>
2021-10-02 16:51:38 +0300

    Add files via upload

---
+++ pozitifNegatif.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 01:57:36 2021
+
+@author: 90545
+"""
+
+sayi = int(input("Sayı giriniz."))
+
+if sayi>0:
+    print("pozitif")
+elif sayi == 0:
+    print("nötr")
+else:
+    print("negatif")
